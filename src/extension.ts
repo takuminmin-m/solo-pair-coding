d2bdaf2f676a19f47ca05b5430a1e199032903e1
takuminmin-m <takuminmin0718@gmail.com>
2023-07-09 00:30:33 +0900

    try completion examples

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { Range } from 'vscode';
+import { Configuration, OpenAIApi } from "openai";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -20,7 +22,103 @@ export function activate(context: vscode.ExtensionContext) {
 	});
 
 	context.subscriptions.push(disposable);
+
+
+	const gptProvider = vscode.languages.registerCompletionItemProvider('plaintext', {
+
+		provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+
+			const gptCompletion = new vscode.CompletionItem('ai coder');
+			getGptResponse("String").then((message) => {
+				if (message) {
+					gptCompletion.insertText = message.content;
+				} else {
+					gptCompletion.insertText = "No response from GPT-3.5";
+				}
+			});
+
+			return [gptCompletion];
+		}
+	});
+
+
+	context.subscriptions.push(gptProvider);
+
+	const inlineProvider: vscode.InlineCompletionItemProvider = {
+		async provideInlineCompletionItems(document, position, context, token) {
+			console.log("inline completion triggered");
+			const regexp = /\/\/ \[(.+?),(.+?)\)(.*?):(.*)/;
+
+			if (position.line <= 0) {
+				return;
+			}
+
+			const result: vscode.InlineCompletionList = {
+				items: [],
+			};
+
+			let offset = 1;
+			while (offset > 0) {
+				if (position.line - offset < 0) {
+					break;
+				}
+
+				const lineBefore = document.lineAt(position.line - offset).text;
+				const matches = lineBefore.match(regexp);
+				if (!matches) {
+					break;
+				}
+				offset++;
+
+				const start = matches[1];
+				const startInt = parseInt(start, 10);
+				const end = matches[2];
+				const endInt =
+					end === '*'
+						? document.lineAt(position.line).text.length
+						: parseInt(end, 10);
+
+				const flags = matches[3];
+				const completeBracketPairs = flags.includes('b');
+				const isSnippet = flags.includes('s');
+				const text = matches[4].replace(/\\n/g, '\n');
+
+				result.items.push({
+					insertText: isSnippet ? new vscode.SnippetString(text) : text,
+					range: new Range(position.line, startInt, position.line, endInt),
+				});
+
+			}
+
+			return result;
+		},
+	};
+
+	vscode.languages.registerInlineCompletionItemProvider({ pattern: '**' }, inlineProvider);
 }
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+async function getGptResponse(code: string) {
+	const configuration = new Configuration({
+		apiKey: "YOUR API KEY",
+		organization: "YOUR ORGANIZATION ID",
+	});
+
+	console.log(configuration);
+
+	const openai = new OpenAIApi(configuration);
+
+	const completion = await openai.createChatCompletion({
+		model: "gpt-3.5-turbo",
+		messages: [{
+			role: "user",
+			content: "Hello world. Please give me a example of JavaScript code that will print 'Hello world' to the console."
+		}]
+	});
+
+	console.log(completion);
+
+	return completion.data.choices[0].message;
+}
